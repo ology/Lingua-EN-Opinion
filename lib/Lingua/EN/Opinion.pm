22925392865806d71ff15c10b2eb923cdf54162f
Gene Boggs <gene.boggs@gmail.com>
2018-03-21 22:33:31 -0700

    Fix typo

--- lib/Lingua/EN/Opinion.pm
+++ lib/Lingua/EN/Opinion.pm
@@ -69,7 +69,7 @@ has text => (
 
 Boolean flag to indicate that word stemming should take place.
 
-For example, "horses" becaomes "horse" and "hooves" becomes "hoof."
+For example, "horses" becomes "horse" and "hooves" becomes "hoof."
 
 This is the proper way to use this module.
 
